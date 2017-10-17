5f8d6de13a911d34d6420b843a688755b247b62e
”Dani” <daniel_franco001@ehu.eus>
2017-10-17 17:13:46 +0200

    1) Memory summary table above memory pieChart 2) Filter to monitor only main processes 3) Added support for mvapich 5) DCRAB finalization ensured 4) Example dcrab_report4.html added

--- src/scripts/dcrab_node_report_functions.sh
+++ src/scripts/dcrab_node_report_functions.sh
@@ -13,21 +13,17 @@
 #
 # ===============================================================================================================
 
+write_initial_values () {
+	# Write memory static values 
+        (
+        flock -e 200
+        sed -i "$mem_piePlot1_nodeMemory_line"'s|\([0-9]*\) GB</td></tr>|'"$node_total_mem"' GB</td></tr>|' $DCRAB_HTML
+        sed -i "$mem_piePlot1_requestedMemory_line"'s|\([0-9]*\) GB</td></tr>|'"$DCRAB_REQ_MEM"' GB</td></tr>|' $DCRAB_HTML
+	) 200>$DCRAB_REPORT_DIR/aux/.dcrab.lockfile
+}
 
-#
-# Args:
-#
-#  1- int     <-- Operation number. (0-CPU,1-MEM)
-#
 write_data () {
-
-        echo "WRITE: $1"
-        echo "cpu_data $cpu_data"
-        echo "mem_data $mem_data"
-	echo "updates: $udpates"
-
-case $1 in
-	0) ## CPU ##
+	### CPU specific change ###
         # Update the plot to insert new process
         if [ "$updates" -gt 0 ]; then
                 for i in $(seq 0 $((updates -1)) ); do
@@ -44,119 +40,163 @@ case $1 in
                 done
         fi
 
-        # Creates a lock to write the data
+	### MEM  specific change ###
+	# Change the color when exceeds memory limit
+        if [ "$exceeded" -eq 1 ] && [ "$changed" -eq 0 ]; then
+                (
+                flock -e 200
+
+                sed -i "$memUsed_addRow_inject_line"'s/#3366CC/#ff0000/' $DCRAB_HTML
+
+                ) 200>$DCRAB_REPORT_DIR/aux/.dcrab.lockfile
+                exceeded=0
+                changed=1
+        # Set the color to normal again
+        elif [ "$exceeded" -eq 0 ] && [ "$changed" -eq 1 ]; then
+                (
+                flock -e 200
+
+                sed -i "$memUsed_addRow_inject_line"'s/#ff0000/#3366CC/' $DCRAB_HTML
+
+                ) 200>$DCRAB_REPORT_DIR/aux/.dcrab.lockfile
+                changed=0
+        fi
+
+
+        # Write data 
         (
         flock -e 200
 
-        sed -i "$cpu_addRow_inject_line"'s/.*/&'"$cpu_data"'/' $DCRAB_HTML
+		### CPU ###
+	        sed -i "$cpu_addRow_inject_line"'s/.*/&'"$cpu_data"'/' $DCRAB_HTML
+
+		### MEM ###
+	        sed -i "$mem_addRow_inject_line"'s/.*/&'"$mem_data"'/' $DCRAB_HTML
+		sed -i "$memUnUsed_addRow_inject_line"'s|\([0-9]*[.]*[0-9]*\)\]|'"$notUtilizedMem"'\]|' $DCRAB_HTML
+		sed -i "$memUsed_addRow_inject_line"'s|\([0-9]*[.]*[0-9]*\)\]|'"$utilizedMem"'\]|' $DCRAB_HTML
+		sed -i "$mem_piePlot1_VmRSS_text_line"'s|\([0-9]*[.]*[0-9]*\) GB</td></tr>|'"$max_RSS_size"' GB</td></tr>|' $DCRAB_HTML
+		sed -i "$mem_piePlot1_VmSize_text_line"'s|\([0-9]*[.]*[0-9]*\) GB</td></tr>|'"$max_vmSize"' GB</td></tr>|' $DCRAB_HTML
 
         ) 200>$DCRAB_REPORT_DIR/aux/.dcrab.lockfile
-	;;
-
-	1) ## MEM ##
-
-        # Creates a lock to write the data
-        (
-        flock -e 200
-
-        sed -i "$mem_addRow_inject_line"'s/.*/&'"$mem_data"'/' $DCRAB_HTML
-	sed -i "$memUnUsed_addRow_inject_line"'s|\([0-9]*\)\]|'"$notUtilizedMem"'\]|g' $DCRAB_HTML
-	sed -i "$memUsed_addRow_inject_line"'s|\([0-9]*\)\]|'"$utilizedMem"'\]|g' $DCRAB_HTML
-
-        ) 200>$DCRAB_REPORT_DIR/aux/.dcrab.lockfile
-	
-	;;
-esac
 }
 
 dcrab_collect_mem_data () {
 
-	# Store the data
+	# Store the data of all the processes 
 	:> $DCRAB_MEM_FILE
-	for pid in ${proc_pids[@]}; do cat /proc/$pid/status 2> /dev/null 1>> $DCRAB_MEM_FILE; done
+	for pid in ${proc_pids[@]}; do cat /proc/$pid/status 2> /dev/null 1 >> $DCRAB_MEM_FILE; done
 	
-	# Collect mem data
-    	vmSize=$(grep VmSize $DCRAB_MEM_FILE | awk '{sum+=$2} END {print sum/1024/1024}')
+	# Collect memory data of the file
+    	vmSize=$(grep VmSize $DCRAB_MEM_FILE | awk '{sum+=$2} END {print sum/1024/1024}') 
+	vmSize=$(printf "%.3f\n" "$vmSize") # 3 decimmals only
 	vmRSS=$(grep VmRSS $DCRAB_MEM_FILE | awk '{sum+=$2} END {print sum/1024/1024}')
-	if [ $(echo "$vmSize > $max_vmSize" | bc) -eq 1 ]; then
-        	max_vmSize=$vmSize
+	vmRSS=$(printf "%.3f\n" "$vmRSS") # 3 decimmals only
+	if [ $(echo "$vmRSS > $max_RSS_size" | bc) -eq 1 ]; then
+        	max_RSS_size=$vmRSS
 	fi
-	utilizedMem=`echo "($max_vmSize * 100)/$DCRAB_REQ_MEM" | bc `
-	notUtilizedMem=`echo "100 - $utilizedMem" | bc `
+	if [ $(echo "$vmSize > $max_vmSize" | bc) -eq 1 ]; then
+                max_vmSize=$vmSize
+        fi
+
+	# Check if exceeds memory requested. The job may be killed by the scheduler.
+	if [ $(echo "$vmRSS < $DCRAB_REQ_MEM" | bc) -eq 1 ]; then
+		utilizedMem=`echo "scale=3; ($max_RSS_size * 100)/$DCRAB_REQ_MEM" | bc `
+		notUtilizedMem=`echo "scale=3; 100 - $utilizedMem" | bc `
+	else
+		notUtilizedMem=0
+                utilizedMem=100
+                exceeded=1
+	fi
 
 	# Construct mem data string
-	mem_data="$mem_data""$node_total_mem, ""$DCRAB_REQ_MEM, ""$max_vmSize, ""$vmSize, ""$vmRSS ],"
+	mem_data="$mem_data""$node_total_mem, ""$DCRAB_REQ_MEM, ""$max_RSS_size, ""$vmSize, ""$vmRSS ],"
 }
 
 dcrab_determine_main_process () {
 
-	# Needed variables	
+	# Init. variables	
         DCRAB_DIFF_TIMESTAMP=0
         DCRAB_MAIN_PIDS=0
-	DCRAB_MAIN_PROCESS_LAST_CHILD_PID=0
+	DCRAB_FIRST_MAIN_PROCESS_PID=""
+	DCRAB_NUMBER_MAIN_PIDS=0
+	DCRAB_MAIN_PROCESS_LAST_CHILD_PID=""
 	DCRAB_CONTROL_PORT_MAIN_NODE="none1"
 	DCRAB_CONTROL_PORT_OTHER_NODE="none2"
-
+	DCRAB_RANGE_PIDs=1
         updates=0
         declare -a upd_proc_name                
 	declare -a proc_pids
 	proc_number=0
-
         # CPU variables
         cpu_data="0,"
-
 	# MEM variables
 	min_utilizedMem=0
+	max_RSS_size=0
 	max_vmSize=0
 	mem_data="[0,"	
+        exceeded=0
+        changed=0
 
 	# MAIN NODE
 	echo "$node_hostname , $DCRAB_NODE_NUMBER"
 	if [ "$DCRAB_NODE_NUMBER" -eq 0 ]; then
 	        IFS=$'\n'
-	        for line in $(ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm,command | sed 's|\s\s*| |g' | grep "\s$DCRAB_USER_ID\s" | grep "Ss")
+		ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm,command | sed 's|\s\s*| |g' | grep " $DCRAB_USER_ID " | grep -v " $DCRAB_DCRAB_PID " > $DCRAB_USER_PROCESSES_FILE
+	        for line in $(cat $DCRAB_USER_PROCESSES_FILE)
 	        do
-			echo "$line"
-	                pid=$(echo "$line" | awk '{print $6}')
-	                pstree $pid | grep -q $DCRAB_JOB_ID
-	                if [ "$?" -eq 0 ]; then 
-				DCRAB_MAIN_PIDS="$pid"
-					
-				# Save last child's pid
-				#DCRAB_MAIN_PROCESS_LAST_CHILD_PID=$(ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm | grep $pid | tail -1 | awk '{printf $6}')
-			#else
-			# To check if there is another processes belong to the job. If consecutive PIDs  --> belong the job
-				#if [ "$DCRAB_MAIN_PROCESS_LAST_CHILD_PID" -ne 0 ]; then
-				#	if [ "$pid" -eq $(DCRAB_MAIN_PROCESS_LAST_CHILD_PID + 1) ]; then
-				#		DCRAB_MAIN_PIDS="$DCRAB_MAIN_PIDS""|""$pid"	
-				#	fi
-				#fi
+			pid=$(echo "$line" | awk '{print $6}')
+			echo "$line"  | grep -q "Ss"
+			if [ "$?" -eq 0 ]; then
+		                pstree $pid | grep -q $DCRAB_JOB_ID
+		                if [ "$?" -eq 0 ]; then 
+					DCRAB_MAIN_PIDS="$pid"
+					DCRAB_FIRST_MAIN_PROCESS_PID=$pid
+					DCRAB_FIRST_MAIN_PROCESS_NAME=$(echo "$line" | awk '{print $8}')
+	
+					# Save last child's pid
+					DCRAB_MAIN_PROCESS_LAST_CHILD_PID=$(cat $DCRAB_USER_PROCESSES_FILE | grep $pid | tail -1 | awk '{printf $6}')
+				
+					break;
+				fi
 			fi
 	        done
 	# REST OF NODES
 	else
+		i=1
 		# Wait until the main node creates control port file
+		echo "Waiting until control_port.txt has been created"
 		while [ ! -f $DCRAB_REPORT_DIR/aux/control_port.txt ]; do 
+			echo "Loop number ($i/$DCRAB_NUMBERS_OF_LOOPS_CONTROL). No control_port.txt created yet. Waiting a bit more . . . "
 			sleep 5
+
+			# Exit DCRAB if no control_port.txt was created  
+			if [ "$i" -eq "$DCRAB_NUMBERS_OF_LOOPS_CONTROL" ]; then
+				echo "Waiting much time to control_port. Exiting DCRAB . . ."
+				exit 1
+			fi
+			i=$((i + 1))
 		done
+		echo "File control_port.txt created!"
 
 		# Wait until the processes of the job start
 	        IFS=$'\n'; i=0
 		DCRAB_CONTROL_PORT_MAIN_NODE=$(cat $DCRAB_REPORT_DIR/aux/control_port.txt)
 		while [ "$DCRAB_CONTROL_PORT_MAIN_NODE" != "$DCRAB_CONTROL_PORT_OTHER_NODE" ]; do
-			echo "Waiting until the process in the node $node_hostname start. -$DCRAB_CONTROL_PORT_MAIN_NODE""- Control-port: -$DCRAB_CONTROL_PORT_OTHER_NODE""-"
+			echo "Waiting until the process in the node $node_hostname start" 
 	
 	                i=$((i + 1))
+
 	                # Wait if it is not the first loop
 	                [[ "$i" -gt 1 ]] && sleep 5
 			
-			for line in $(ps axo stat,euid,ruid,sess,ppid,pid,pcpu,command | sed 's|\s\s*| |g' | grep "\s$DCRAB_USER_ID\s" | grep "Ss")
+			for line in $(ps axo stat,euid,ruid,sess,ppid,pid,pcpu,command | sed 's|\s\s*| |g' | grep " $DCRAB_USER_ID " | grep "Ss")
 		        do
 				DCRAB_CONTROL_PORT_OTHER_NODE=`echo ${line#*control-port} | awk '{print $1}'`
-				echo " DCRAB_CONTROL_PORT_OTHER_NODE: $DCRAB_CONTROL_PORT_OTHER_NODE"
 				if [ "$DCRAB_CONTROL_PORT_OTHER_NODE" == "$DCRAB_CONTROL_PORT_MAIN_NODE" ]; then
 					pid=$(echo "$line" | awk '{print $6}')
-	                                DCRAB_MAIN_PIDS="$pid"	
+	                                DCRAB_MAIN_PIDS="$pid"
+					DCRAB_FIRST_MAIN_PROCESS_PID=$pid
+                                        DCRAB_FIRST_MAIN_PROCESS_NAME=$(echo "$line" | awk '{print $8}')	
 					break
 				fi
 			done
@@ -165,32 +205,33 @@ dcrab_determine_main_process () {
 	fi
 
         # Initialize data file
-        for line in $(ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm,command | sed 's|\s\s*| |g' | grep -E "$DCRAB_MAIN_PIDS")
+        for line in $(ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm,command | sed 's|\s\s*| |g' | grep " $DCRAB_USER_ID " | grep -v " $DCRAB_DCRAB_PID " | grep -E "$DCRAB_MAIN_PIDS")
         do
 		# Get information of the process
                 pid=$(echo "$line" | awk '{print $6}')
                 cpu=$(echo "$line" | awk '{print $7}')
                 commandName=$(echo "$line" | awk '{print $8}')
-		
-		# Save in the data file
-                echo "$pid $commandName" >> $DCRAB_PROCESS_FILE
+		if [ $(echo "$cpu > $cpu_threshold" | bc) -eq 1 ]; then
+			# Save in the data file
+        	        echo "$pid $commandName" >> $DCRAB_JOB_PROCESSES_FILE
 
-		# If it is the control process the main node must store it. Needed for multinode statistics. 
-                echo $line | grep -q "control-port"
-                if [ "$?" -eq 0 ] && [ "$DCRAB_NODE_NUMBER" -eq 0 ]; then
-                        DCRAB_CONTROL_PORT_MAIN_NODE=$(echo ${line#*control-port} | awk '{print $1}')
-                        echo "$DCRAB_CONTROL_PORT_MAIN_NODE" > $DCRAB_REPORT_DIR/aux/control_port.txt
-                fi
+			# If it is the control process the main node must store it. Needed for multinode statistics. 
+	                echo $line | grep -q "control-port"
+	                if [ "$?" -eq 0 ] && [ "$DCRAB_NODE_NUMBER" -eq 0 ]; then
+	                        DCRAB_CONTROL_PORT_MAIN_NODE=$(echo ${line#*control-port} | awk '{print $1}')
+	                        echo "$DCRAB_CONTROL_PORT_MAIN_NODE" > $DCRAB_REPORT_DIR/aux/control_port.txt
+	                fi
 
-                # CPU data
-		upd_proc_name[$updates]=$commandName
-                cpu_data="$cpu_data $cpu,"
-
-		# MEM data
-		proc_pids[$proc_number]=$pid
-		proc_number=$((proc_number + 1))
-        	        
-		updates=$((updates + 1))
+        	        # CPU data
+			upd_proc_name[$updates]=$commandName
+	                cpu_data="$cpu_data $cpu,"
+	
+			# MEM data
+			proc_pids[$proc_number]=$pid
+			proc_number=$((proc_number + 1))
+	        	        
+			updates=$((updates + 1))
+		fi
         done
 
         # Get time
@@ -199,47 +240,90 @@ dcrab_determine_main_process () {
         # CPU data. Remove the last comma
         cpu_data=${cpu_data%,*}
         cpu_data="[$cpu_data ],"
-        write_data 0
 
 	# MEM data
 	dcrab_collect_mem_data
-	write_data 1	
+
+	write_data 	
 }
 
 dcrab_update_data () {
-
-        # CPU data
-        cpu_data="["
 	
-	# MEM data
-	mem_data="["
-
+        # Init. variables
         updates=0
         IFS=$'\n'
-
-        # Collect the data
-        :> $DCRAB_PROCESS_FILE.tmp
-        ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm,command | sed 's|\s\s*| |g' | grep -E "$DCRAB_MAIN_PIDS" >> $DCRAB_PROCESS_FILE.tmp
         DCRAB_M2_TIMESTAMP=`date +"%s"`
         DCRAB_DIFF_TIMESTAMP=$((DCRAB_M2_TIMESTAMP - DCRAB_M1_TIMESTAMP))
-
         # CPU data      
         cpu_data="$DCRAB_DIFF_TIMESTAMP,"
-
         # MEM data
         mem_data="[$DCRAB_DIFF_TIMESTAMP,"
 
+        # Collect the data
+	ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm,command | sed 's|\s\s*| |g' | grep " $DCRAB_USER_ID " | grep -v " $DCRAB_DCRAB_PID " > $DCRAB_USER_PROCESSES_FILE
+        cat $DCRAB_USER_PROCESSES_FILE | grep -E "$DCRAB_MAIN_PIDS" > $DCRAB_JOB_PROCESSES_FILE.tmp
+
+	# To check if there is some processes that aren't in the process tree of the main node but belongs the job (this case occurs with mvapich)
+	diff -u $DCRAB_JOB_PROCESSES_FILE.tmp $DCRAB_USER_PROCESSES_FILE | grep "^\+" | awk 'NR>1 {print}' | sed 's|+||' > $DCRAB_USER_PROCESSES_FILE.tmp
+	if [ $(cat $DCRAB_USER_PROCESSES_FILE.tmp | wc -l) -gt 0 ]; then
+
+		# Renew DCRAB_MAIN_PROCESS_LAST_CHILD_PID variable
+                DCRAB_MAIN_PROCESS_LAST_CHILD_PID=$(cat $DCRAB_USER_PROCESSES_FILE | grep $DCRAB_FIRST_MAIN_PROCESS_PID | tail -1 | awk '{printf $6}')
+		DCRAB_LAST_CHILD_NEXT_VALID_PID=$((DCRAB_MAIN_PROCESS_LAST_CHILD_PID + 1))
+                i=0; found=0
+                while [ "$found" -eq 0 ]; do
+                	kill -0 $DCRAB_LAST_CHILD_NEXT_VALID_PID >> /dev/null 2>&1
+                        [[ $? -eq 0 ]] && found=1 || DCRAB_LAST_CHILD_NEXT_VALID_PID=$((DCRAB_LAST_CHILD_NEXT_VALID_PID + 1))
+                done
+
+	        for line in $(cat $DCRAB_USER_PROCESSES_FILE.tmp)
+	        do
+			pid=$(echo "$line" | awk '{print $6}')
+                        if [ "$DCRAB_MAIN_PROCESS_LAST_CHILD_PID" != "" ] && [ "$DCRAB_FIRST_MAIN_PROCESS_PID" != "0" ]; then
+                        	# To check that is not child of any of the main processes
+                        	pstree -p $DCRAB_FIRST_MAIN_PROCESS_PID | grep -q "$pid"
+                                if [ $? -ne 0 ] && 
+				   [ "$pid" -le $((DCRAB_MAIN_PROCESS_LAST_CHILD_PID + DCRAB_RANGE_PIDs)) ] && 
+				   [ "$pid" -ge $((DCRAB_MAIN_PROCESS_LAST_CHILD_PID - DCRAB_RANGE_PIDs)) ]; then
+                               		DCRAB_MAIN_PIDS="$DCRAB_MAIN_PIDS""|""$pid"
+                                        DCRAB_NUMBER_MAIN_PIDS=$((DCRAB_NUMBER_MAIN_PIDS + 1))
+                                        echo "Another main process confirmed. PID:$pid, COMMAND: $(echo "$line" | awk '{print $8}')"
+	
+			                # If the new process is the control process. Needed for multinode statistics.
+                        		echo $line | grep -q "control-port"
+		                        if [ "$?" -eq 0 ] && [ "$DCRAB_NODE_NUMBER" -eq 0 ]; then
+		                                DCRAB_CONTROL_PORT_MAIN_NODE=$(echo ${line#*control-port} | awk '{print $1}')
+		                                echo "$DCRAB_CONTROL_PORT_MAIN_NODE" > $DCRAB_REPORT_DIR/aux/control_port.txt
+                		        fi
+				elif [ "$?" -ne "0" ] && [ "$pid" == "$DCRAB_LAST_CHILD_NEXT_VALID_PID" ]; then
+					DCRAB_MAIN_PIDS="$DCRAB_MAIN_PIDS""|""$pid"
+                                        DCRAB_NUMBER_MAIN_PIDS=$((DCRAB_NUMBER_MAIN_PIDS + 1))
+                                        echo "Another main process confirmed. PID:$pid, COMMAND: $(echo "$line" | awk '{print $8}')"
+
+					# If the new process is the control process. Needed for multinode statistics.
+                                        echo $line | grep -q "control-port"
+                                        if [ "$?" -eq 0 ] && [ "$DCRAB_NODE_NUMBER" -eq 0 ]; then
+                                                DCRAB_CONTROL_PORT_MAIN_NODE=$(echo ${line#*control-port} | awk '{print $1}')
+                                                echo "$DCRAB_CONTROL_PORT_MAIN_NODE" > $DCRAB_REPORT_DIR/aux/control_port.txt
+                                        fi
+                                fi
+                        fi		                
+
+		done
+	fi
+
         lastEmptyValue=0
         i=1
-        for line in $(cat $DCRAB_PROCESS_FILE)
+	# Check first old processes
+        for line in $(cat $DCRAB_JOB_PROCESSES_FILE)
         do
                 pid=$(echo "$line" | awk '{print $1}')
                 commandName=$(echo "$line" | awk '{print $2}')
-                auxLine=`cat $DCRAB_PROCESS_FILE.tmp | grep -n "$commandName" | awk '{if ($6 == '"$pid"'){print}}'`
+                auxLine=`cat $DCRAB_JOB_PROCESSES_FILE.tmp | grep -n "$commandName" | awk '{if ($6 == '"$pid"'){print}}'`
                 if [ "$auxLine" != "" ]; then
                         lineNumber=$(echo $auxLine | cut -d: -f1)
                         cpu=$(echo "$auxLine" | awk '{print $7}')
-                        sed -i "$lineNumber""d" $DCRAB_PROCESS_FILE.tmp
+                        sed -i "$lineNumber""d" $DCRAB_JOB_PROCESSES_FILE.tmp
                 else
                         lastEmptyValue=$i
                         cpu=" "
@@ -251,29 +335,33 @@ dcrab_update_data () {
         done
 
         # Check if there are new processes
-        for line in $(cat $DCRAB_PROCESS_FILE.tmp)
+        for line in $(cat $DCRAB_JOB_PROCESSES_FILE.tmp)
         do
                 pid=$(echo "$line" | awk '{print $6}')
-                cpu=$(echo "$line" | awk '{print $7}')
-                commandName=$(echo "$line" | awk '{print $8}')
-                sed -i '1s|^|'"$pid $commandName"'\n|' $DCRAB_PROCESS_FILE
-                upd_proc_name[$updates]=$commandName
+	        cpu=$(echo "$line" | awk '{print $7}')
+	        commandName=$(echo "$line" | awk '{print $8}')
+	
+		
+		if [ $(echo "$cpu > $cpu_threshold" | bc) -eq 1 ]; then
+	                sed -i '1s|^|'"$pid $commandName"'\n|' $DCRAB_JOB_PROCESSES_FILE
+	                upd_proc_name[$updates]=$commandName
 
-		# If the new process is the control process. Needed for multinode statistics.
-		echo $line | grep -q "control-port"
-		if [ "$?" -eq 0 ] && [ "$DCRAB_NODE_NUMBER" -eq 0 ]; then
-			DCRAB_CONTROL_PORT_MAIN_NODE=$(echo ${line#*control-port} | awk '{print $1}')
-			echo "$DCRAB_CONTROL_PORT_MAIN_NODE" > $DCRAB_REPORT_DIR/aux/control_port.txt
+			# If the new process is the control process. Needed for multinode statistics.
+			echo $line | grep -q "control-port"
+			if [ "$?" -eq 0 ] && [ "$DCRAB_NODE_NUMBER" -eq 0 ]; then
+				DCRAB_CONTROL_PORT_MAIN_NODE=$(echo ${line#*control-port} | awk '{print $1}')
+				echo "$DCRAB_CONTROL_PORT_MAIN_NODE" > $DCRAB_REPORT_DIR/aux/control_port.txt
+			fi
+
+	                # CPU data
+	                cpu_data=`echo $cpu_data | sed "s|^$DCRAB_DIFF_TIMESTAMP,|$DCRAB_DIFF_TIMESTAMP, $cpu,|"`
+
+	                # MEM data
+	                proc_pids[$proc_number]=$pid
+	                proc_number=$((proc_number + 1))
+
+	                updates=$((updates + 1))
 		fi
-
-                # CPU data
-                cpu_data=`echo $cpu_data | sed "s|^$DCRAB_DIFF_TIMESTAMP,|$DCRAB_DIFF_TIMESTAMP, $cpu,|"`
-
-                # MEM data
-                proc_pids[$proc_number]=$pid
-                proc_number=$((proc_number + 1))
-
-                updates=$((updates + 1))
         done
 
 	# CPU data
--- src/scripts/dcrab_startDataCollection.sh
+++ src/scripts/dcrab_startDataCollection.sh
@@ -13,8 +13,7 @@
 #
 #  1- aux/env.txt  	<-- Used to store environment
 #  2- int	   	<-- Node number 
-#  3- int   	   	<-- Time of desynchronization. To try not overlapping the writes on the .html file.
-#  4- dcrab_$node.log	<-- Log file
+#  3- dcrab_$node.log	<-- Log file
 # 
 # Do NOT execute manually. DCRAB will start it automatically
 #
@@ -22,7 +21,7 @@
 
 
 # Redirect all the output to DCRAB log file
-exec >> $4 ; exec 2>&1
+exec >> $3 ; exec 2>&1
 echo "--- DCRAB `hostname` log ---" 
 
 # Sets environment
@@ -36,9 +35,14 @@ source $DCRAB_BIN/scripts/dcrab_node_report_functions.sh
 
 # Necessary variables
 DCRAB_NODE_NUMBER=$2
+DCRAB_DCRAB_PID=$$
 node_hostname=`hostname`
 node_hostname_mod=`echo $node_hostname | sed 's|-||g'`
-DCRAB_PROCESS_FILE=$DCRAB_REPORT_DIR/data/$node_hostname/dcrab_process_$node_hostname.txt
+DCRAB_WAIT_TIME_CONTROL=180 # 3 minutes
+DCRAB_SLEEP_TIME_CONTROL=5
+DCRAB_NUMBERS_OF_LOOPS_CONTROL=$(( DCRAB_WAIT_TIME_CONTROL / DCRAB_SLEEP_TIME_CONTROL ))
+DCRAB_USER_PROCESSES_FILE=$DCRAB_REPORT_DIR/data/$node_hostname/dcrab_user_processes_$node_hostname.txt
+DCRAB_JOB_PROCESSES_FILE=$DCRAB_REPORT_DIR/data/$node_hostname/dcrab_job_processes_$node_hostname.txt
 DCRAB_MEM_FILE=$DCRAB_REPORT_DIR/data/$node_hostname/dcrab_mem_$node_hostname.txt
 
 # Save the line to inject CPU data
@@ -48,15 +52,23 @@ addColumn_data_line=`grep -n -m 1 "\/\* $node_hostname_mod addColumn space \*\/"
 # CPU
 cpu_addRow_inject_line=$((addRow_data_line + 2))
 cpu_addColumn_inject_line=$((addColumn_data_line + 1))
+cpu_threshold="5.0"
 
 # MEM
 mem_addRow_inject_line=$((addRow_data_line + 6))
 memUsed_addRow_inject_line=$((addRow_data_line + 10))
 memUnUsed_addRow_inject_line=$((addRow_data_line + 11))
 node_total_mem=`free -g | grep "Mem" | awk ' {printf $2}'`
+mem_piePlot_color_line=$(grep -n -m 1 "var mem2_options =" | cut -f1 -d:)
+mem_piePlot_color_line=$((mem_piePlot_color_line + 4))
+mem_piePlot1_div_line=$(grep -n -m 1 "id='plot1_mem_$node_hostname_mod" $DCRAB_HTML | cut -f1 -d:)
+mem_piePlot1_nodeMemory_line=$((mem_piePlot1_div_line + 4))
+mem_piePlot1_requestedMemory_line=$((mem_piePlot1_div_line + 5))
+mem_piePlot1_VmRSS_text_line=$((mem_piePlot1_div_line + 6))
+mem_piePlot1_VmSize_text_line=$((mem_piePlot1_div_line + 7))
+write_initial_values
 
-# Sleep first time to desynchronize with other nodes
-sleep "$3"
+# Determines the main processes of the job and initializes html file first time 
 dcrab_determine_main_process 
 
 
@@ -74,14 +86,17 @@ while [ 1 ]; do
 	# Update and collect data
 	dcrab_update_data 
 
-	# Insert CPU data in the main .html page
-	write_data 0
+	# Insert collected data in the main .html page
+	write_data 
 
-	# Insert MEM data in the main .html page
-	write_data 1
+	# To finish DCRAB if the main process has finished. This avoids DCRAB continues running if
+	# the scheduler kills the job before the execution of 'dcrab finish'
+	kill -0 $DCRAB_FIRST_MAIN_PROCESS_PID >> /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		echo "DCRAB terminated. First main process '$DCRAB_FIRST_MAIN_PROCESS_NAME, PID: $DCRAB_FIRST_MAIN_PROCESS_PID' has been killed"
+		break;
+	fi
 
 	loopNumber=$((loopNumber + 1))
 done
-
-
 
