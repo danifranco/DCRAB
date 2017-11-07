c6672d2b4cd7081e0b2170b81076e5df958da6ee
”Dani” <daniel_franco001@ehu.eus>
2017-11-07 13:32:10 +0100

    1) Added memory monitorization. 2) Restructure html file and add mem plots.

--- src/scripts/dcrab_config.sh
+++ src/scripts/dcrab_config.sh
@@ -23,8 +23,8 @@ dcrab_generate_html (){
 
 	plot_width=800
 	plot_height=600
-
-	DCRAB_HTML=$DCRAB_REPORT_DIR/dcrab_report.html
+	space_width=30
+	addedBorder=6
 
 	# Copy necessary files to generate html
 	cp $DCRAB_BIN/aux/htmlResources/* $DCRAB_REPORT_DIR/aux/htmlResources
@@ -36,29 +36,46 @@ dcrab_generate_html (){
 	printf "%s \n" "<script type=\"text/javascript\"> " >> $DCRAB_HTML
 
 	# Load packages and callbacks for plot functions
-	printf "%s \n" "google.charts.load('visualization', '1', { 'packages': ['corechart'] });" >> $DCRAB_HTML
-        printf "%s \n" "google.charts.setOnLoadCallback(plot_cpu); " >> $DCRAB_HTML
+	printf "%s \n" "google.charts.load('current', {'packages':['corechart']}); " >> $DCRAB_HTML
+        printf "%s \n" "google.charts.setOnLoadCallback(plot_all); " >> $DCRAB_HTML
 
-	################# CPU plot function #################
-        printf "%s \n" "function plot_cpu() { " >> $DCRAB_HTML
+	################# Plot function #################
+        printf "%s \n" "function plot_all() { " >> $DCRAB_HTML
 
 	# Data of each nodes
         for node in $DCRAB_NODES_MOD
         do
-                printf "%s \n" "var data_$node = new google.visualization.DataTable(); " >> $DCRAB_HTML
-		printf "%s \n" "data_$node.addColumn('number', 'Execution Time (s)');" >> $DCRAB_HTML
+		# CPU 
+                printf "%s \n" "var cpu_data_$node = new google.visualization.DataTable(); " >> $DCRAB_HTML
+		printf "%s \n" "cpu_data_$node.addColumn('number', 'Execution Time (s)');" >> $DCRAB_HTML
+		
 		printf "%s \n" "/* $node addColumn space */" >> $DCRAB_HTML
 		printf "\n" >> $DCRAB_HTML
 
 		# Variables to store data declaration (where the nodes will write collected data)
 		printf "%s \n" "/* $node addRow space */" >> $DCRAB_HTML
+		
+		# CPU
 		printf "%s \n" "var cpu_$node = [" >> $DCRAB_HTML
 		printf "\n"  >> $DCRAB_HTML
 		printf "%s \n" "];" >> $DCRAB_HTML
-	        printf "%s \n" "data_$node.addRows(cpu_$node);" >> $DCRAB_HTML
+
+		# MEM
+                printf "%s \n" "var mem1_$node = google.visualization.arrayToDataTable([" >> $DCRAB_HTML
+		printf "%s \n" "['Execution Time (s)', 'Node Memory', 'Requested Memory', 'Consumed Memory Peak (VmPeak)', 'Consumed Memory (VmSize)', 'Resident Memory (VmRSS)']," >> $DCRAB_HTML
+                printf "\n"  >> $DCRAB_HTML
+                printf "%s \n" "]);" >> $DCRAB_HTML
+                printf "%s \n" "var mem2_$node = google.visualization.arrayToDataTable([" >> $DCRAB_HTML
+		printf "%s \n" "['Mem class', 'Percentage']," >> $DCRAB_HTML
+                printf "%s \n" "['Used memory', 0]," >> $DCRAB_HTML
+                printf "%s \n" "['Not utilized memory', 100]," >> $DCRAB_HTML
+                printf "%s \n" "]);" >> $DCRAB_HTML
+		
+	        printf "%s \n" "cpu_data_$node.addRows(cpu_$node);" >> $DCRAB_HTML
 	done
 
-        printf "%s \n" "var options = {" >> $DCRAB_HTML
+	# CPU
+        printf "%s \n" "var cpu_options = {" >> $DCRAB_HTML
         printf "%s \n" "title: 'CPU Utilization'," >> $DCRAB_HTML
         printf "%s \n" "width: '$plot_width'," >> $DCRAB_HTML
         printf "%s \n" "height: '$plot_height'," >> $DCRAB_HTML
@@ -85,24 +102,83 @@ dcrab_generate_html (){
         printf "%s \n" "chartArea: {  width: \"70%\", height: \"80%\" }," >> $DCRAB_HTML
         printf "%s \n" "};" >> $DCRAB_HTML
 
+	# MEM
+        printf "%s \n" "var mem1_options = {  " >> $DCRAB_HTML
+        printf "%s \n" "title : 'Memory Utilization', " >> $DCRAB_HTML
+        printf "%s \n" "vAxis: {title: 'GB'}, " >> $DCRAB_HTML
+        printf "%s \n" "hAxis: {title: 'Time (s)'}, " >> $DCRAB_HTML
+        printf "%s \n" "width: $plot_width,  " >> $DCRAB_HTML
+        printf "%s \n" "height: $plot_height,  " >> $DCRAB_HTML
+        printf "%s \n" "axes: {  " >> $DCRAB_HTML
+        printf "%s \n" "x: {  " >> $DCRAB_HTML
+        printf "%s \n" "0: {side: 'top'}  " >> $DCRAB_HTML
+        printf "%s \n" "}  " >> $DCRAB_HTML
+        printf "%s \n" "},  " >> $DCRAB_HTML
+        printf "%s \n" "};  " >> $DCRAB_HTML
+
+        printf "%s \n" "var mem2_options = {" >> $DCRAB_HTML
+        printf "%s \n" "title: 'Requested memory usage'," >> $DCRAB_HTML
+        printf "%s \n" "width: $((plot_width / 2))," >> $DCRAB_HTML
+        printf "%s \n" "height: $((plot_height / 2))," >> $DCRAB_HTML
+        printf "%s \n" "colors: ['#3366CC', '#109618']," >> $DCRAB_HTML
+        printf "%s \n" "is3D: true" >> $DCRAB_HTML
+        printf "%s \n" "};" >> $DCRAB_HTML
+
+
         for node in $DCRAB_NODES_MOD
         do
-	        printf "%s \n" "var chart_$node = new google.visualization.LineChart(document.getElementById('plot_cpu_$node'));"  >> $DCRAB_HTML
-        	printf "%s \n" "chart_$node.draw(data_$node, options);  " >> $DCRAB_HTML
+		# CPU
+	        printf "%s \n" "var cpu_chart_$node = new google.visualization.LineChart(document.getElementById('plot_cpu_$node'));"  >> $DCRAB_HTML
+        	printf "%s \n" "cpu_chart_$node.draw(cpu_data_$node, cpu_options);  " >> $DCRAB_HTML
+
+		# MEM
+	        printf "%s \n" "var mem1_chart_$node = new google.visualization.AreaChart(document.getElementById('plot1_mem_$node'));"  >> $DCRAB_HTML
+	        printf "%s \n" "var mem2_chart_$node = new google.visualization.PieChart(document.getElementById('plot2_mem_$node'));"  >> $DCRAB_HTML
+
+        	printf "%s \n" "mem1_chart_$node.draw(mem1_$node, mem1_options);  " >> $DCRAB_HTML
+        	printf "%s \n" "mem2_chart_$node.draw(mem2_$node, mem2_options);  " >> $DCRAB_HTML
         done
+
 	printf "%s \n" "}" >> $DCRAB_HTML
-        ################# END CPU plot function #################
+        ################# END plot function #################
 
         printf "%s \n" "</script>" >> $DCRAB_HTML
 
 	################# Style #################
         printf "%s \n" "<style>" >> $DCRAB_HTML
-        printf "%s \n" "div#cpu {" >> $DCRAB_HTML
+        printf "%s \n" "body {" >> $DCRAB_HTML
+        printf "%s \n" "margin: 0px;" >> $DCRAB_HTML
+        printf "%s \n" "}" >> $DCRAB_HTML
+        printf "%s \n" "p {" >> $DCRAB_HTML
+        printf "%s \n" "font-family: monospace;" >> $DCRAB_HTML
+        printf "%s \n" "}" >> $DCRAB_HTML
+        printf "%s \n" "table {" >> $DCRAB_HTML
+        printf "%s \n" "border-collapse: collapse;" >> $DCRAB_HTML
+        printf "%s \n" "margin: 15px;" >> $DCRAB_HTML
+        printf "%s \n" "margin-top: 2px;" >> $DCRAB_HTML
+        printf "%s \n" "margin-bottom: 2px;" >> $DCRAB_HTML
+        printf "%s \n" "}" >> $DCRAB_HTML
+        printf "%s \n" "tr {" >> $DCRAB_HTML
+        printf "%s \n" "border: 1px solid rgba(254, 254, 254, 0.3);" >> $DCRAB_HTML
+        printf "%s \n" "}" >> $DCRAB_HTML
+        printf "%s \n" ".overflowDivs {" >> $DCRAB_HTML
         printf "%s \n" "overflow:auto;" >> $DCRAB_HTML
+        printf "%s \n" "}" >> $DCRAB_HTML
+        printf "%s \n" ".header2 {" >> $DCRAB_HTML
+        printf "%s \n" "width: $(( (plot_width * 2) - (plot_width / 2) + (space_width + addedBorder) ))px;" >> $DCRAB_HTML
+        printf "%s \n" "border: 1px solid rgba(254, 254, 254, 0.3);" >> $DCRAB_HTML
+        printf "%s \n" "float: left;" >> $DCRAB_HTML
+        printf "%s \n" "text-align: center;" >> $DCRAB_HTML 
+        printf "%s \n" "font-family: 'Roboto', sans-serif;" >> $DCRAB_HTML
+        printf "%s \n" "font-size: 16px;" >> $DCRAB_HTML
+        printf "%s \n" "color: #fff;" >> $DCRAB_HTML
+        printf "%s \n" "text-transform: uppercase;" >> $DCRAB_HTML
+        printf "%s \n" "vertical-align: middle;" >> $DCRAB_HTML
+        printf "%s \n" "background-color: rgba(255,255,255,0.3);" >> $DCRAB_HTML
         printf "%s \n" "}" >> $DCRAB_HTML
         printf "%s \n" "" >> $DCRAB_HTML
         printf "%s \n" ".header {" >> $DCRAB_HTML
-        printf "%s \n" "width: 800px;" >> $DCRAB_HTML
+        printf "%s%s \n" "width: $plot_width" "px;" >> $DCRAB_HTML
         printf "%s \n" "border: 1px solid rgba(254, 254, 254, 0.3);" >> $DCRAB_HTML
         printf "%s \n" "float: left;" >> $DCRAB_HTML
         printf "%s \n" "text-align: center;" >> $DCRAB_HTML
@@ -114,14 +190,19 @@ dcrab_generate_html (){
         printf "%s \n" "background-color: rgba(255,255,255,0.3);" >> $DCRAB_HTML
         printf "%s \n" "}" >> $DCRAB_HTML
         printf "%s \n" "" >> $DCRAB_HTML
-        printf "%s \n" ".cpu_plot{" >> $DCRAB_HTML
+        printf "%s \n" ".plot{" >> $DCRAB_HTML
         printf "%s \n" "border: 1px solid rgba(254, 254, 254, 0.3);" >> $DCRAB_HTML
         printf "%s \n" "}" >> $DCRAB_HTML
         printf "%s \n" "" >> $DCRAB_HTML
         printf "%s \n" ".space {" >> $DCRAB_HTML
-        printf "%s \n" "width: 30px;" >> $DCRAB_HTML
+        printf "%s%s \n" "width: $space_width" "px;" >> $DCRAB_HTML
         printf "%s \n" "border: 0px;" >> $DCRAB_HTML
         printf "%s \n" "background-color: none;" >> $DCRAB_HTML
+        printf "%s \n" "}" >> $DCRAB_HTML
+        printf "%s \n" "#foot {" >> $DCRAB_HTML
+        printf "%s \n" "background-color: rgba(255,255,255,0.3);" >> $DCRAB_HTML
+        printf "%s \n" "padding: 1px 80px;" >> $DCRAB_HTML
+        printf "%s \n" "margin-top: 180px;" >> $DCRAB_HTML
         printf "%s \n" "}" >> $DCRAB_HTML
 	printf "%s \n" "</style>" >> $DCRAB_HTML
 	################# END Style #################
@@ -129,38 +210,83 @@ dcrab_generate_html (){
 	# Job summary
         printf "%s \n" "<body background=\"./aux/htmlResources/background.png\">" >> $DCRAB_HTML
         printf "%s \n" "<center><h1>DCRAB REPORT - JOB $DCRAB_JOB_ID </h1></center>" >> $DCRAB_HTML
+        printf "%s \n" "<div style=\"padding-left: 15px;\">" >> $DCRAB_HTML
+        printf "%s \n" "<p>" >> $DCRAB_HTML
         printf "%s \n" "<br><br>" >> $DCRAB_HTML
-        printf "%s \n" "Name of the job: $DCRAB_JOBNAME <br>" >> $DCRAB_HTML
-        printf "%s \n" "JOBID: $DCRAB_JOB_ID <br>" >> $DCRAB_HTML
-        printf "%s \n" "User: $USER <br>" >> $DCRAB_HTML
-        printf "%s \n" "Working directory: $DCRAB_WORKDIR <br>" >> $DCRAB_HTML
-        printf "%s \n" "Node list: <br>" >> $DCRAB_HTML
+        printf "%s \n" "<b><u>Name of the job</u></b>: $DCRAB_JOBNAME <br>" >> $DCRAB_HTML
+        printf "%s \n" "<b><u>JOBID</u></b>: $DCRAB_JOB_ID <br>" >> $DCRAB_HTML
+        printf "%s \n" "<b><u>User</u></b>: $USER <br>" >> $DCRAB_HTML
+        printf "%s \n" "<b><u>Working directory</u></b>: $DCRAB_WORKDIR <br>" >> $DCRAB_HTML
+        printf "%s \n" "<b><u>Node list</u></b>: <br>" >> $DCRAB_HTML
         printf "%s \n" "$DCRAB_NODES <br>" >> $DCRAB_HTML
 	printf "%s \n" "<br><br><br><br>" >> $DCRAB_HTML
+        printf "%s \n" "</p>" >> $DCRAB_HTML
+        printf "%s \n" "</div>" >> $DCRAB_HTML
 
 	################# CPU plots #################
-        printf "%s \n" "<div id="cpu">" >> $DCRAB_HTML
+        printf "%s \n" "<div class=\"overflowDivs\">" >> $DCRAB_HTML
         printf "%s \n" "<table>" >> $DCRAB_HTML
         printf "%s \n" "<tr>" >> $DCRAB_HTML
+	i=1
         for node in $DCRAB_NODES
         do      
 	        printf "%s \n" "<td><div class=\"header\">$node</div></td>" >> $DCRAB_HTML
-		printf "%s \n" "<td><div class=\"space\"></div></td>" >> $DCRAB_HTML
+		[[ "$i" -ne "$DCRAB_NNODES" ]] && printf "%s \n" "<td><div class=\"space\"></div></td>" >> $DCRAB_HTML
+		i=$((i + 1))
         done
         printf "%s \n" "</tr>" >> $DCRAB_HTML
         printf "%s \n" "</table>" >> $DCRAB_HTML
         printf "%s \n" "<table>" >> $DCRAB_HTML
         printf "%s \n" "<tr>" >> $DCRAB_HTML
+	i=1
         for node in $DCRAB_NODES_MOD
         do         
-		printf "%s \n" "<td><div class=\"cpu_plot\" id='plot_cpu_$node'></div></td>" >> $DCRAB_HTML
-                printf "%s \n" "<td><div class=\"space\"></div></td>" >> $DCRAB_HTML
+		printf "%s \n" "<td><div class=\"plot\" id='plot_cpu_$node'></div></td>" >> $DCRAB_HTML
+                [[ "$i" -ne "$DCRAB_NNODES" ]] && printf "%s \n" "<td><div class=\"space\"></div></td>" >> $DCRAB_HTML
+		i=$((i + 1))
         done
         printf "%s \n" "</tr>" >> $DCRAB_HTML
         printf "%s \n" "</table>" >> $DCRAB_HTML
         printf "%s \n" "</div>" >> $DCRAB_HTML
-	################# END CPU plots #################
+	################# END CPU plots ##############
 
+	printf "%s \n" "<br><br><br>" >> $DCRAB_HTML
+
+	################# MEM plots #################
+        printf "%s \n" "<div class=\"overflowDivs\">" >> $DCRAB_HTML
+        printf "%s \n" "<table>" >> $DCRAB_HTML
+        printf "%s \n" "<tr>" >> $DCRAB_HTML
+	i=1
+        for node in $DCRAB_NODES
+        do      
+	        printf "%s \n" "<td><div class=\"header2\">$node</div></td>" >> $DCRAB_HTML
+		[[ "$i" -ne "$DCRAB_NNODES" ]] && printf "%s \n" "<td><div class=\"space\"></div></td>" >> $DCRAB_HTML
+		i=$((i + 1))
+        done
+        printf "%s \n" "</tr>" >> $DCRAB_HTML
+        printf "%s \n" "</table>" >> $DCRAB_HTML
+        printf "%s \n" "<table>" >> $DCRAB_HTML
+        printf "%s \n" "<tr>" >> $DCRAB_HTML
+	i=1
+        for node in $DCRAB_NODES_MOD
+        do         
+		printf "%s \n" "<td><div class=\"plot\" id='plot1_mem_$node'></div></td>" >> $DCRAB_HTML
+                printf "%s \n" "<td><div class=\"space\"></div></td>" >> $DCRAB_HTML
+		printf "%s \n" "<td><div class=\"plot\" id='plot2_mem_$node'></div></td>" >> $DCRAB_HTML
+                [[ "$i" -ne "$DCRAB_NNODES" ]] && printf "%s \n" "<td><div class=\"space\"></div></td>" >> $DCRAB_HTML
+		i=$((i + 1))
+        done
+        printf "%s \n" "</tr>" >> $DCRAB_HTML
+        printf "%s \n" "</table>" >> $DCRAB_HTML
+        printf "%s \n" "</div>" >> $DCRAB_HTML
+	################# END CPU plots ##############
+
+	printf "%s \n" "<div id='foot'>" >> $DCRAB_HTML
+	printf "%s \n" "<p>" >> $DCRAB_HTML
+	printf "%s \n" "<pre class=\"tab\">" >> $DCRAB_HTML
+	printf "%s \n" "<a href=\"http://dipc.ehu.es/\" target=\"_blank\"><img src=\"./aux/htmlResources/dipc_logo.png\" alt=\"DIPC logo\" width=\"90\" height=\"40\" align=\"middle\"></a>                      Copyright &copy; 2017 DIPC (Donostia International Physics Center)             <div class=\"vl\" style=\"display: inline; border-left: 2px solid black; height: 10px;\"></div>            <a href=\"http://dipc.ehu.es/cc/computing_resources/index.html\" target=\"_blank\"  style=\"text-decoration: none; color: black\"><b>Wiki</b></a>            <div class=\"vl\" style=\"display: inline; border-left: 2px solid black; height: 10px;\"></div>            <a href=\"http://dipc.ehu.es/\" target=\"_blank\"  style=\"text-decoration: none; color: black\"><b>DIPC Home Page</b></a>             <div class=\"vl\" style=\"display: inline; border-left: 2px solid black; height: 10px;\"></div> </pre>" >> $DCRAB_HTML
+	printf "%s \n" "</p>" >> $DCRAB_HTML
+	printf "%s \n" "</div>" >> $DCRAB_HTML
         printf "%s \n" "</body> " >> $DCRAB_HTML
         printf "%s \n" "</html> " >> $DCRAB_HTML
 }
@@ -175,6 +301,8 @@ dcrab_check_scheduler () {
                 DCRAB_JOBNAME=$SLURM_JOB_NAME
                 DCRAB_NODES=`scontrol show hostname $SLURM_NODELIST`
                 DCRAB_NNODES=`scontrol show hostname $SLURM_NODELIST | wc -l`
+		DCRAB_JOBFILE=`ps $PPID  | awk '{printf $6}'`
+		DCRAB_REQ_MEM=0
         elif [ -n "$PBS_NODEFILE" ]; then
                 DCRAB_SCHEDULER=pbs
                 DCRAB_JOB_ID=$PBS_JOBID
@@ -182,13 +310,17 @@ dcrab_check_scheduler () {
                 DCRAB_JOBNAME=$PBS_JOBNAME
                 DCRAB_NODES=`cat $PBS_NODEFILE | sort | uniq`
                 DCRAB_NNODES=`cat $PBS_NODEFILE | sort | uniq | wc -l`
+		DCRAB_JOBFILE=`ps $PPID  | awk '{printf $6}'`
+		DCRAB_REQ_MEM=`cat $DCRAB_JOBFILE | grep "\-l mem=" | cut -d= -f2 | sed 's/[^0-9]*//g'`
         else
-                DCRAB_SCHEDULER=none
+                DCRAB_SCHEDULER="none"
                 DCRAB_JOB_ID=`date +%s`
                 DCRAB_WORKDIR=.
                 DCRAB_JOBNAME="$USER.job"
                 DCRAB_NODES=`hostname -a`
                 DCRAB_NNODES=1
+		DCRAB_JOBFILE="none"
+		DCRAB_REQ_MEM=0
         fi
 }
 
@@ -244,7 +376,7 @@ dcrab_start_data_collection () {
 		cont=$((DCRAB_TIME_HOWOFTEN * i))
 
                 COMMAND="$DCRAB_BIN/scripts/dcrab_startDataCollection.sh $DCRAB_WORKDIR/$DCRAB_REPORT_DIR/aux/env.txt $i $cont $DCRAB_WORKDIR/$DCRAB_LOG_DIR/dcrab_$node.log & echo \$!"
-		echo "COMMAND____= $COMMAND" 
+
                 # Hay que poner la key, sino pide password
                 DCRAB_PIDs[$i]=`ssh -n $node PATH=$PATH $COMMAND | tail -n 1 `
 
@@ -264,5 +396,6 @@ dcrab_init_variables () {
         export DCRAB_LOG_DIR=$DCRAB_REPORT_DIR/log
         export DCRAB_MAIN_JOB_PID=$PPID
 	export DCRAB_USER_ID=`id -u $USER`
+	export DCRAB_HTML=$DCRAB_REPORT_DIR/dcrab_report.html
 }
 
--- src/scripts/dcrab_node_report_functions.sh
+++ src/scripts/dcrab_node_report_functions.sh
@@ -14,15 +14,98 @@
 # ===============================================================================================================
 
 
+#
+# Args:
+#
+#  1- int     <-- Operation number. (0-CPU,1-MEM)
+#
+write_data () {
+
+        echo "WRITE: $1"
+        echo "cpu_data $cpu_data"
+        echo "mem_data $mem_data"
+
+case $1 in
+	0) ## CPU ##
+        # Update the plot to insert new process
+        if [ "$updates" -gt 0 ]; then
+                for i in $(seq 0 $((updates -1)) ); do
+                        echo "write, loop $i"
+                        # Creates a lock to write the data
+                        (
+                        flock -e 200
+
+                        # Add new process entry in the plot. Specifically in the second position of the every data array
+                        sed -i "$cpu_addRow_inject_line"'s|\[\([0-9]*\),|\[\1, ,|g' $DCRAB_HTML
+                        # Add new process column
+                        sed -i "$cpu_addColumn_inject_line""s|^|cpu_data_$node_hostname_mod.addColumn('number', '${upd_proc_name[$i]}'); |" $DCRAB_HTML
+
+                        ) 200>$DCRAB_REPORT_DIR/aux/.dcrab_$node_hostname_mod.lockfile
+                done
+        fi
+
+        # Creates a lock to write the data
+        (
+        flock -e 200
+
+        sed -i "$cpu_addRow_inject_line"'s/.*/&'"$cpu_data"'/' $DCRAB_HTML
+
+        ) 200>$DCRAB_REPORT_DIR/aux/.dcrab_$node_hostname_mod.lockfile
+	;;
+
+	1) ## MEM ##
+
+        # Creates a lock to write the data
+        (
+        flock -e 200
+
+        sed -i "$mem_addRow_inject_line"'s/.*/&'"$mem_data"'/' $DCRAB_HTML
+	sed -i "$memUnUsed_addRow_inject_line"'s|\([0-9]*\)\]|'"$notUtilizedMem"'\]|g' $DCRAB_HTML
+	sed -i "$memUsed_addRow_inject_line"'s|\([0-9]*\)\]|'"$utilizedMem"'\]|g' $DCRAB_HTML
+
+        ) 200>$DCRAB_REPORT_DIR/aux/.dcrab_$node_hostname_mod.lockfile
+	
+	;;
+esac
+}
+
+dcrab_collect_mem_data () {
+
+	# Store the data
+	:> $DCRAB_MEM_FILE
+	for pid in ${proc_pids[@]}; do cat /proc/$pid/status 2> /dev/null 1>> $DCRAB_MEM_FILE; done
+	
+	# Collect mem data
+    	vmSize=$(grep VmSize $DCRAB_MEM_FILE | awk '{sum+=$2} END {print sum/1024/1024}')
+	vmRSS=$(grep VmRSS $DCRAB_MEM_FILE | awk '{sum+=$2} END {print sum/1024/1024}')
+	if [ $(echo "$vmSize > $max_vmSize" | bc) -eq 1 ]; then
+        	max_vmSize=$vmSize
+	fi
+	utilizedMem=`echo "($max_vmSize * 100)/$DCRAB_REQ_MEM" | bc `
+	notUtilizedMem=`echo "100 - $utilizedMem" | bc `
+
+	# Construct mem data string
+	mem_data="$mem_data""$node_total_mem, ""$DCRAB_REQ_MEM, ""$max_vmSize, ""$vmSize, ""$vmRSS ],"
+}
+
 dcrab_determine_main_process () {
 
+
+	# Needed variables	
         DCRAB_DIFF_TIMESTAMP=0
         DCRAB_MAIN_PID=0
         updates=0
         declare -a upd_proc_name                
+	declare -a proc_pids
+	proc_number=0
 
-        # CPU data
+        # CPU variables
         cpu_data="0,"
+
+	# MEM variables
+	min_utilizedMem=0
+	max_vmSize=0
+	mem_data="[0,"	
 
         # Determine the main process
         IFS=$'\n'
@@ -37,57 +120,73 @@ dcrab_determine_main_process () {
         # Initialize data file
         for line in $(ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm | sed 's|\s\s*| |g' | grep "$DCRAB_MAIN_PID")
         do
+		# Get information of the process
                 pid=$(echo "$line" | awk '{print $6}')
                 cpu=$(echo "$line" | awk '{print $7}')
                 commandName=$(echo "$line" | awk '{print $8}')
-                echo "$pid $commandName" >> $1
-                upd_proc_name[$updates]=$commandName
+		
+		# Save in the data file
+                echo "$pid $commandName" >> $DCRAB_PROCESS_FILE
 
                 # CPU data
+		upd_proc_name[$updates]=$commandName
                 cpu_data="$cpu_data $cpu,"
 
-                updates=$((updates + 1))
+		# MEM data
+		proc_pids[$proc_number]=$pid
+		proc_number=$((proc_number + 1))
+        	        
+		updates=$((updates + 1))
         done
 
         # Get time
         DCRAB_M1_TIMESTAMP=`date +"%s"`
 
-        # Remove the last comma
+        # CPU data. Remove the last comma
         cpu_data=${cpu_data%,*}
         cpu_data="[$cpu_data ],"
+        write_data 0
 
-        write_data $addRow_inject_line $addColumn_inject_line $cpu_data
+	# MEM data
+	dcrab_collect_mem_data
+	write_data 1	
 }
 
 dcrab_update_data () {
 
         # CPU data
         cpu_data="["
+	
+	# MEM data
+	mem_data="["
 
         updates=0
         IFS=$'\n'
 
         # Collect the data
-        :> $1.tmp
-        ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm | sed 's|\s\s*| |g' | grep "$DCRAB_MAIN_PID" >> $1.tmp
+        :> $DCRAB_PROCESS_FILE.tmp
+        ps axo stat,euid,ruid,sess,ppid,pid,pcpu,comm | sed 's|\s\s*| |g' | grep "$DCRAB_MAIN_PID" >> $DCRAB_PROCESS_FILE.tmp
         DCRAB_M2_TIMESTAMP=`date +"%s"`
         DCRAB_DIFF_TIMESTAMP=$((DCRAB_M2_TIMESTAMP - DCRAB_M1_TIMESTAMP))
 
         # CPU data      
         cpu_data="$DCRAB_DIFF_TIMESTAMP,"
 
+        # MEM data
+        mem_data="[$DCRAB_DIFF_TIMESTAMP,"
+
         lastEmptyValue=0
         i=1
-        for line in $(cat $1)
+        for line in $(cat $DCRAB_PROCESS_FILE)
         do
                 pid=$(echo "$line" | awk '{print $1}')
                 commandName=$(echo "$line" | awk '{print $2}')
 
-                pos=`cat $1.tmp | grep -n "$pid" | grep "$commandName"`
+                pos=`cat $DCRAB_PROCESS_FILE.tmp | grep -n "$pid" | grep "$commandName"`
                 if [ "$?" -eq 0 ]; then
                         lineNumber=$(echo $pos | cut -d: -f1)
                         cpu=$(echo "$pos" | awk '{print $7}')
-                        sed -i "$lineNumber""d" $1.tmp
+                        sed -i "$lineNumber""d" $DCRAB_PROCESS_FILE.tmp
                 else
                         lastEmptyValue=$i
                         cpu=" "
@@ -95,61 +194,46 @@ dcrab_update_data () {
 
                 # CPU data
                 cpu_data="$cpu_data $cpu,"
-
+	
                 i=$((i + 1))
         done
 
         # Check if there are new processes
-        for line in $(cat $1.tmp)
+        for line in $(cat $DCRAB_PROCESS_FILE.tmp)
         do
                 pid=$(echo "$line" | awk '{print $6}')
                 cpu=$(echo "$line" | awk '{print $7}')
                 commandName=$(echo "$line" | awk '{print $8}')
-                sed -i '1s|^|'"$pid $commandName"'\n|' $1
+                sed -i '1s|^|'"$pid $commandName"'\n|' $DCRAB_PROCESS_FILE
                 upd_proc_name[$updates]=$commandName
 
                 # CPU data
                 cpu_data=`echo $cpu_data | sed "s|^$DCRAB_DIFF_TIMESTAMP,|$DCRAB_DIFF_TIMESTAMP, $cpu,|"`
 
+                # MEM data
+                proc_pids[$proc_number]=$pid
+                proc_number=$((proc_number + 1))
+
                 updates=$((updates + 1))
         done
 
+	# CPU data
         # To avoid cpu_data termine like '0, ]', which means that the last process has been terminated, and will cause an error in the plot 
         # So we put a 0 value instead of the ' ' (space) character 
         if [ $((lastEmptyValue + 1)) -eq $i ]; then
                 cpu_data=${cpu_data%,*}
                 cpu_data="$cpu_data""0,"
         fi
-
         # Remove the last comma
         cpu_data=${cpu_data%,*}
         cpu_data="[$cpu_data],"
+	
+        # MEM data
+        dcrab_collect_mem_data
 }
 
-write_data () {
 
-	# Update the plot to insert new process
-	if [ "$updates" -gt 0 ]; then
-		for i in $(seq 0 $((updates -1)) ); do
-			echo "write, loop $i"
-   		        # Creates a lock to write the data
-		        (
-		        flock -e 200
 
-			# Add new process entry in the plot. Specifically in the second position of the every data array
-		        sed -i "$1"'s|\[\([0-9]*\),|\[\1, ,|g' $DCRAB_REPORT_DIR/dcrab_report.html      
-			# Add new process column
- 			sed -i "$2""s|^|data_$node_hostname_mod.addColumn('number', '${upd_proc_name[$i]}'); |" $DCRAB_REPORT_DIR/dcrab_report.html
 
-		        ) 200>$DCRAB_REPORT_DIR/aux/.dcrab_$node_hostname_mod.lockfile
-		done
-	fi
 
-        # Creates a lock to write the data
-        (
-        flock -e 200
 
-        sed -i "$1"'s/.*/&'"$3"'/' $DCRAB_REPORT_DIR/dcrab_report.html
-
-        ) 200>$DCRAB_REPORT_DIR/aux/.dcrab_$node_hostname_mod.lockfile
-}
