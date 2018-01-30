f3044a433bfb22f1006888f9885126c1bf905218
”Dani” <daniel_franco001@ehu.eus>
2018-01-30 16:51:00 +0100

    1) Comprobation minor change

--- aux/dcrab_comprobation.sh
+++ aux/dcrab_comprobation.sh
@@ -38,7 +38,7 @@ if [ "$1" != "" ]; then
                         position=$(grep -n "#PBS" $1 | tail -1 | cut -d':' -f1)
 
                         # Insert DCRAB lines
-                        sed -i "$position"'s|$|\nexport DCRAB_BIN=/scratch/dfranco/3-dcrab/src\nexport PATH=$PATH:$DCRAB_BIN\ndcrab istart\n|' $1
+                        sed -i "$position"'s|$|\nexport DCRAB_BIN=/scratch/administracion/admin/dcrab/software/src\nexport PATH=$PATH:$DCRAB_BIN\ndcrab istart\n|' $1
                         echo "dcrab ifinish" >> $1		
 		fi
 	fi
